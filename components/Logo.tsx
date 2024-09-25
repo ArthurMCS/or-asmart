f9543cfaf9c2839dea29abdb4460176c2f5792dc
Arthur Moreira <arthurmcs96@gmail.com>
2024-09-25 17:02:36 -0300

    feat: cria a navbar

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,22 @@
-import { PiggyBank } from 'lucide-react'
+import { Wallet } from 'lucide-react'
 import React from 'react'
 
 function Logo() {
   return (
     <a href='/' className='flex items-center gap-2'>
-        <PiggyBank className='stroke h-11 w-11 stroke-green-500 stroke-[1.5]' />
+        <Wallet className='stroke h-11 w-11 stroke-green-500 stroke-[1.5]' />
+        <p className='bg-gradient-to-r 
+        from-green-400 
+        to-green-600 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+            OrçaSmart
+        </p>
+    </a>
+  )
+}
+
+export function LogoMobile() {
+  return (
+    <a href='/' className='flex items-center gap-2'>
         <p className='bg-gradient-to-r 
         from-green-400 
         to-green-600 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
